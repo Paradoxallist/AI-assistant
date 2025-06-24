b4347e08b0b73b6bfebf465a7cfe200622fe52b7
Yevhenii Liashenko <paradoxrial@gmail.com>
2025-06-24 21:52:25 +0200

    Add init_db script with path

---
+++ utils/path.py
@@ -0,0 +1,8 @@
+import os
+from config.settings import DB_PATH
+
+def resolve_from_root(relative_path):
+    root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+    return os.path.join(root, relative_path)
+
+DB_ABS_PATH = resolve_from_root(DB_PATH)
